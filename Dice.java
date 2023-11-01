50a7faa650008c794c5663798c578a128a2ac38e
Sangharsh11 <patilsangharsh11@gmail.com>
2023-11-01 19:30:33 +0000

    returned the values

--- Dice.java
+++ Dice.java
@@ -4,16 +4,18 @@ public class Dice {
 
     private Random random;
 
-    public void roll() {
+    public Dice() {
         random = new Random();
-        // Rolling two dice and returning the values of each dice
+    }
+
+    public int[] roll() {
+
         int[] diceValues = new int[2];
         diceValues[0] = random.nextInt(6) + 1; // Value of the first dice
         diceValues[1] = random.nextInt(6) + 1; // Value of the second dice
 
-        // return diceValues;
-        System.out.println(diceValues[0]);
-        System.out.println(diceValues[1]);
+        return diceValues;
+
     }
 
 }
