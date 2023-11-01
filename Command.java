b4ba7de3b7293642b77a45278b44b2918ad3ca2d
AtiqueUCD <145844898+AtiqueUCD@users.noreply.github.com>
2023-11-01 19:53:15 +0000

    Added command file

---
+++ Command.java
@@ -0,0 +1,22 @@
+public class Command {
+    
+    int[] diceRoll = new int[2];
+
+    void acceptCommand(String command)
+    {
+        switch(command)
+        {
+            case "R":
+            case "r":
+                /*Roll dice */
+
+            break;
+        }
+    }
+
+    int[] getDiceRoll()
+    {
+        return diceRoll;
+    }
+
+}
