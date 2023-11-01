d6b48ae6921c3fc6caefcdc7be76792895c11f2a
Emile Aydar <114087019+xXMagIkZzR4mBOXx@users.noreply.github.com>
2023-11-01 21:27:33 +0100

    Add files via upload

---
+++ Spike.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Spike extends ArrayList<Checker>{
+    private int position;
+
+    public Spike(int position){
+        super();
+        this.position = position;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public int nbColoredChecker(String color){
+        int nbColored = 0;
+        for(int i=0; i<this.size(); i++){
+            if(this.get(i).getColor().equals(color)){
+                nbColored++;
+            }
+        }
+        return nbColored;
+    }
+
+    public boolean addChecker(Checker checker){
+        boolean isAdded = false;
+        String OppositeColor = "RESET";
+        if(checker.getColor().equals("RED")){
+            OppositeColor = "BLACK";
+        }
+        else if(checker.getColor().equals("BLACK")){
+            OppositeColor = "RED";
+        }
+
+        if(nbColoredChecker(OppositeColor)<2){
+            this.add(checker);
+            isAdded=true;
+        }else{
+            System.out.println("The move is invalid");
+        }
+        return isAdded;
+    }
+
+    public boolean deleteChecker(Checker checker){
+        boolean isDeleted=false;
+        if(!this.isEmpty()){
+            if(nbColoredChecker(checker.getColor())>0){
+                int index =0;
+                for(int i=0; i<this.size(); i++){
+                    if(this.get(i).getColor().equals(checker.getColor())){
+                        index = i;
+                    }
+                }
+                this.remove(index);
+                isDeleted=true;
+            }
+        }
+        return isDeleted;
+    }
+
+
+}
