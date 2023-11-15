747b1fcb8fbb9ef06e93539d8556f4da9bad548f
AtiqueUCD <145844898+AtiqueUCD@users.noreply.github.com>
2023-11-15 21:20:14 +0000

    Added hint command

--- Command.java
+++ Command.java
@@ -11,6 +11,13 @@ public class Command extends Dice{
                 /* Roll the dice */
                 diceRoll = roll();
                 break;
+            case "hint":
+                System.out.println("==============================");
+                System.out.println("Commands to play:");
+                System.out.println("1. R/r to roll");
+                System.out.println("2. Q/q to quit");
+                System.out.println("==============================");
+            break;
             default:
                 System.out.println("Invalid command. Please enter 'R/r' to roll the dice or 'Q/q' to Quit");
                 break;
