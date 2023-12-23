1fa69390cd8e0d5ab055c13482ba58325851c0e5
AtiqueUCD <145844898+AtiqueUCD@users.noreply.github.com>
2023-12-23 12:24:23 +0000

    Added the Turn class JUnit testing with the player testing

--- PlayerTest.java
+++ PlayerTest.java
@@ -3,46 +3,71 @@ import org.junit.jupiter.api.*;
 
 public class PlayerTest {
     
-    private Player player;
+    private Player PlayerOne;
+    private Player PlayerTwo;
+    private Turn turn;
 
     @BeforeEach
     void setUp()
     {
-        player = new Player("Sam Winchester");
-        player.setNumCheckers(100);
-        player.setScore(69);
-        player.setTurn(true);
-        player.setMoveBlock();
-        player.increaseScore();
+        PlayerOne = new Player("Sam Winchester");
+        PlayerTwo = new Player("Dean Winchester");
+        turn = new Turn();
+
+
+        PlayerOne.setNumCheckers(100);
+        PlayerOne.setScore(69);
+        PlayerOne.setTurn(true);
+        PlayerOne.setMoveBlock();
+        PlayerOne.increaseScore();
+
+        PlayerTwo.setNumCheckers(234);
+        PlayerTwo.setScore(70);
+        PlayerTwo.setTurn(false);
+        PlayerTwo.resetMoveBlock();
+        PlayerTwo.decreaseScore();
     }
 
     @Test
     void testName()
     {
-        assertEquals("Sam Winchester", player.getName());
+        assertEquals("Sam Winchester", PlayerOne.getName());
+        assertEquals("Dean Winchester", PlayerTwo.getName());
     }
 
     @Test
     void testNumCheckers()
     {
-        assertEquals(100, player.getNumCheckers());
+        assertEquals(100, PlayerOne.getNumCheckers());
+        assertEquals(234, PlayerTwo.getNumCheckers());
     }
 
     @Test
     void testScore()
     {
-        assertEquals(70, player.getScore());
+        assertEquals(70, PlayerOne.getScore());
+        assertEquals(69, PlayerTwo.getScore());
     }
 
     @Test
     void testTurnStatus()
     {
-        assertEquals(true, player.isTurn());
+        assertEquals(true, PlayerOne.isTurn());
+        assertEquals(false, PlayerTwo.isTurn());
     }
 
     @Test
     void testBlockedMove()
     {
-        assertEquals(true,player.getmoveBlocked());
+        assertEquals(true,PlayerOne.getmoveBlocked());
+        assertEquals(false,PlayerTwo.getmoveBlocked());
+    }
+
+    @Test
+    void testTurnClass()
+    {
+        assertEquals(false,turn.getBlockedmove(PlayerOne, PlayerTwo));
+        assertEquals(PlayerTwo,turn.getCurrentPlayer(PlayerOne, PlayerTwo));
+        assertEquals(Checker.BLACK,turn.getCurrentPlayerColor());
     }
 }
